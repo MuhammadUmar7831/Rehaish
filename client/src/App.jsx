97d0da18d64a7b6b2e7c8fe252654e61d407cb3f
Muhammad Umar <mu8494759@gmail.com>
2024-05-14 23:05:28 +0500

    project setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center w-[100vw] h-[100vh]">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
