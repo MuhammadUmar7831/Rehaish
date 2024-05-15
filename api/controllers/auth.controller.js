ef91344a3f49b216c37bfc34930a05a559ead4dd
Muhammad Umar <mu8494759@gmail.com>
2024-05-15 19:09:14 +0500

    Add error handling middleware to auth controller and  custom error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const salt = 10;
   const hashedPassword = bcryptjs.hashSync(password, salt);
@@ -10,6 +11,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).send(newUser);
   } catch (error) {
-    res.status(500).send(error.message);
+    next(errorHandler(400, 'From function'));
   }
 };
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -25,3 +25,13 @@ app.listen(port, () => {
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).send({
+    success: false,
+    statusCode,
+    message,
+  });
+});
