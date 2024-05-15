9f6fe6fbdf69773a4b112c91ff05892d74ae6e86
Muhammad Umar <mu8494759@gmail.com>
2024-05-15 15:01:46 +0500

    test api route created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -18,3 +19,5 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+app.use("/user", userRouter)
