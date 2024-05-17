17348d239196b382f7a71344986453121f11ae42
Muhammad Umar <mu8494759@gmail.com>
2024-05-17 18:00:45 +0500

    update user route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,6 +5,23 @@ export const test = (req, res) => {
 };
 
 export const getUser = async (req, res) => {
-  const user = await User.findById({ _id: req.userId });
-  res.status(200).send({ success: true, user });
+  try {
+    const user = await User.findById({ _id: req.userId });
+    res.status(200).send({ success: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { name, avatar } = req.body;
+  try {
+    const user = await User.findById(req.userId);
+    user.name = name;
+    user.avatar = avatar;
+    await user.save();
+    res.status(200).send({ success: true, user });
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUser, test } from '../controllers/user.controller.js';
+import { getUser, test, updateUser } from '../controllers/user.controller.js';
 import { authenticate } from '../middleware/authenticate.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.get('/getUser', authenticate, getUser);
+router.get('/updateUser', authenticate, updateUser);
 
 export default router;
