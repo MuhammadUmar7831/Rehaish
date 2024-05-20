36b31b0c9c1f9b429103f5f383435ba4500b4b01
Muhammad Umar <mu8494759@gmail.com>
2024-05-20 23:58:43 +0500

    getUserListing route added further unregistered userRef error handeled

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,8 +1,32 @@
+import { errorHandler } from "../errors/error.js";
 import Listing from "../models/listing.model.js";
+import User from "../models/user.model.js";
 
 export const createListing = async (req, res, next) => {
   try {
+    const userId = req.userId;
+    const validUser = await User.findById(userId);
+    if (validUser === null) {
+      return next(errorHandler(404, "user not found!"));
+    }
     const listing = await Listing.create(req.body);
+    return res.status(201).send({ success: true, listing });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+    const validUser = await User.findById(userId);
+    if (validUser === null) {
+      return next(errorHandler(404, "user not found!"));
+    }
+    const listing = await Listing.find({ userRef: userId });
+    if (listing.length === 0){
+      return next(errorHandler(404, 'no listing found'));
+    }
     return res.status(201).send({ success: true, listing });
   } catch (error) {
     next(error);
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { authenticate } from '../middleware/authenticate.js';
-import {createListing} from '../controllers/listing.controller.js'
+import {createListing, getUserListing} from '../controllers/listing.controller.js'
 
 const router = express.Router();
 
 router.post('/create', authenticate, createListing);
+router.get('/getUserListing', authenticate, getUserListing);
 
 export default router;
